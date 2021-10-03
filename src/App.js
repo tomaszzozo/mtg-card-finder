7d3343e99115ae4904dc55332c9010b9ed88b977
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2021-10-03 16:25:08 +0200

    Added basic elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tłumacz kart MTG</h1>
+      <p>Wpisz kod karty z lewego dolnego rogu</p>
+      <input placeholder="001/999" type="text"></input>
     </div>
   );
 }
